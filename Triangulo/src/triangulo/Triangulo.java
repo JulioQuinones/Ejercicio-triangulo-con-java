9c7791b6e805a11745174b8172a819df1f91d22a
Julio Quiñones <53541736+JulioQuinones@users.noreply.github.com>
2019-11-22 22:43:57 -0500

    Add files via upload

---
+++ Triangulo/src/triangulo/Triangulo.java
@@ -0,0 +1,14 @@
+
+package triangulo;
+
+public class Triangulo {
+    public static void main(String[] args) {
+        for(int i =1;i <=10; i++){ //filas
+            for(int j =1; j<=i;j++){//columnas
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
